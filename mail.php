86b63b4364cd36ddde8b2fa6ef18206ff1296851
Chandrika Deb <chandrikadeb7@gmail.com>
2021-05-29 13:04:07 +0000

    contact form mail changes

--- mail.php
+++ mail.php
@@ -1,16 +1,36 @@
 <?php
-if(isset( $_POST['inputName']))
-$name = $_POST['inputName'];
-if(isset( $_POST['inputEmail']))
-$email = $_POST['inputEmail'];
-if(isset( $_POST['inputMessage']))
-$message = $_POST['inputMessage'];
-if(isset( $_POST['inputPhone']))
-$phone = $_POST['inputPhone'];
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'Contact Form';
+    $to = "chandrikadeb7@gmail.com";
+    $subject = 'Email from Moner Hoichoi';
 
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "chandrikadeb7@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $phone, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    // Check if name has been entered
+    if (!$_POST['name']) {
+        $errName = 'Please enter your name';
+    }
+
+    // Check if email has been entered and is valid
+    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errEmail = 'Please enter a valid email address';
+    }
+
+    //Check if message has been entered
+    if (!$_POST['message']) {
+        $errMessage = 'Please enter your message';
+    }
+
+    // If there are no errors, send the email
+    if (!$errName && !$errEmail && !$errMessage) {
+        if (mail ($to, $subject, $body, $from)) {
+            $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+        } else {
+            $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+        }
+    }
+}
 ?>
